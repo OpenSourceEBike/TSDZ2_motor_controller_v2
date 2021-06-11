777cf4356dc87fd67cbca7ad516be68dd3166769
casainho <casainho@gmail.com>
2021-06-11 12:23:27 +0100

    Added information about all the signals on the 6 existing opamps

--- pins.h
+++ pins.h
@@ -8,10 +8,23 @@
 
 #pragma once
 
+// LM258
+// opamp pins 1, 2 and 3, is configured with as a unity gain buffer, providing at output a 2.5 volts signal (probably for the offset voltage used by the other MCP6022 opamps measuring the phase currents)
+// opamp pins 7, 6 and 5, is used to amplify the torque sensor signal.
+
+// MCP6022 1 (near shunt resistor):
+// opamp pins 1, 2 and 3, shunt resistor measurement, amplification factor 10, 2.5 volts offset
+// opamp pins 1, 2 and 3, shunt resistor measurement, amplification factor -11
+
+// MCP6022 2:
+// opamp pins 1, 2 and 3, yellow phase resistor measurement, amplification factor 10, 2.5 volts offset
+// opamp pins 1, 2 and 3, blue phase resistor measurement, amplification factor 10, 2.5 volts offset
+
 // Current shunt: P2.8
-// there is power resistor of 0.003 ohms and then a Non-inverting Operational Amplifier Configuration, with a gain of 11 (Rf = 10k and R2 = 1k)
+// there is power resistor of 0.003 ohms and then a Non-inverting Operational Amplifier Configuration (MCP6022, ouput pin 7), with a gain of 11 (Rf = 10k and R2 = 1k)
 // voltage on this pin is 0 volts when motor is not running
 // VCC = 5 volts and considering ADC is 12 bits mode
 #define CURRENT_SHUNT_ADC_TO_AMPS                       (0.037) // 1 / (0.033 / (5 / 4095))
 #define CURRENT_SHUNT__PORT                             2
 #define CURRENT_SHUNT__PIN                              8
+   
