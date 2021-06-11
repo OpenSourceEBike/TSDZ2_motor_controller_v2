5a6bbc6f6013b2868fd4633b2ad158334f3c3f4b
casainho <casainho@gmail.com>
2021-06-11 14:22:08 +0100

    Corrected info about opamp signals

--- pins.h
+++ pins.h
@@ -13,15 +13,15 @@
 // opamp pins 7, 6 and 5, is used to amplify the torque sensor signal.
 
 // MCP6022 1 (near shunt resistor):
-// opamp pins 1, 2 and 3, shunt resistor measurement, amplification factor 10, 2.5 volts offset
-// opamp pins 1, 2 and 3, shunt resistor measurement, amplification factor -11
+// opamp pins 1, 2 and 3, green phase resistor measurement, amplification factor 10, 2.5 volts offset
+// opamp pins 7, 6 and 5, shunt resistor measurement, amplification factor -11
 
 // MCP6022 2:
 // opamp pins 1, 2 and 3, yellow phase resistor measurement, amplification factor 10, 2.5 volts offset
-// opamp pins 1, 2 and 3, blue phase resistor measurement, amplification factor 10, 2.5 volts offset
+// opamp pins 7, 6 and 5, blue phase resistor measurement, amplification factor 10, 2.5 volts offset
 
 // Current shunt: P2.8
-// there is power resistor of 0.003 ohms and then a Non-inverting Operational Amplifier Configuration (MCP6022, ouput pin 7), with a gain of 11 (Rf = 10k and R2 = 1k)
+// there is power resistor of 0.003 ohms and then a Non-inverting Operational Amplifier Configuration (MCP6022, ouput pin 7), with a gain of -11 (Rf = 10k and R2 = 1k)
 // voltage on this pin is 0 volts when motor is not running
 // VCC = 5 volts and considering ADC is 12 bits mode
 #define CURRENT_SHUNT_ADC_TO_AMPS                       (0.037) // 1 / (0.033 / (5 / 4095))
